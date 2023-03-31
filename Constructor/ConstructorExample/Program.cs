488ad8427b400918a6aa658ae141bf5958e56d28
Bibhuti Nath <bibhuti.nath@atkinsglobal.com>
2023-03-31 10:04:17 +0530

    Constructor and Static class

--- Constructor/ConstructorExample/Program.cs
+++ Constructor/ConstructorExample/Program.cs
@@ -1,8 +1,46 @@
 ﻿using System;
 
-public class ConstructorExample
-{
+public class ConstructorExample{
+
     public static void Main(){
-        Console.WriteLine("Hello World!.");
+        Students student=new Students(1, "ABC");
+
+        //student.PrintStudentDetails();
+
+
+        StudentInfo.CurrentStudent = student;
+        StudentInfo.PrintCurrentStudentDetail();
+
+        Console.ReadKey();
+    }  
+     
+}
+
+public static class StudentInfo
+{
+    public static Students CurrentStudent { get; set; }
+    
+    public static void PrintCurrentStudentDetail()
+    {
+        CurrentStudent.PrintStudentDetails();
     }
+    
+    
+}
+
+public class Students{
+    public Students(int rollNumber, string name)
+    {
+        RollNumber=rollNumber;
+        Name= name;
+    }
+    public int  RollNumber { get; set; }   
+    
+    public string Name { get; set; }
+
+    public void PrintStudentDetails(){
+        Console.WriteLine($"{RollNumber}- {Name}");
+    }
+    
+    
 }
