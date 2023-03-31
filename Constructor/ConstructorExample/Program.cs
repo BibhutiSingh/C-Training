8728c08a70a12e481508cee66c9da74f118477ab
Bibhuti Nath <bibhuti.nath@atkinsglobal.com>
2023-03-31 10:24:43 +0530

    Null types

--- Constructor/ConstructorExample/Program.cs
+++ Constructor/ConstructorExample/Program.cs
@@ -3,12 +3,15 @@
 public class ConstructorExample{
 
     public static void Main(){
+        int? a = 10;
+           int? b=null;
+
+
+        Console.WriteLine($"Sub of A + B ={(a ?? 1)  +  (b ?? 1)}");
         Students student=new Students(1, "ABC");
 
-        //student.PrintStudentDetails();
 
-
-        StudentInfo.CurrentStudent = student;
+        //StudentInfo.CurrentStudent = student;
         StudentInfo.PrintCurrentStudentDetail();
 
         Console.ReadKey();
@@ -22,13 +25,25 @@ public static class StudentInfo
     
     public static void PrintCurrentStudentDetail()
     {
+        if(CurrentStudent is null){
+            Console.WriteLine("No student assigned to current student property.");
+        }
+        else{
         CurrentStudent.PrintStudentDetails();
+        }
     }
     
     
 }
+public interface IStudent
+{
+    public int RollNumber { get; set; }
+    public string Name { get; set; }
+    public void PrintStudentDetails();    
+    
+}
 
-public class Students{
+public class Students : IStudent{
     public Students(int rollNumber, string name)
     {
         RollNumber=rollNumber;
